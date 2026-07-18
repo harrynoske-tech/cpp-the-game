7172a922c6ea920ea9b47884863f704b5a192482
harrynoske-tech <harry.noske@gmail.com>
2026-07-18 16:11:39 +1000

    Update PreloadScene.js

--- scenes/PreloadScene.js
+++ scenes/PreloadScene.js
@@ -1,1 +1,37 @@
+class PreloadScene extends Phaser.Scene {
+    constructor() {
+        super("PreloadScene");
+    }
 
+    preload() {
+        const { width, height } = this.scale;
+
+        const loadingText = this.add.text(
+            width / 2,
+            height / 2 - 30,
+            "Loading...",
+            {
+                fontSize: "28px",
+                color: "#ffffff"
+            }
+        ).setOrigin(0.5);
+
+        const progressText = this.add.text(
+            width / 2,
+            height / 2 + 20,
+            "0%",
+            {
+                fontSize: "20px",
+                color: "#ffffff"
+            }
+        ).setOrigin(0.5);
+
+        this.load.on("progress", (value) => {
+            progressText.setText(`${Math.round(value * 100)}%`);
+        });
+    }
+
+    create() {
+        this.scene.start("MenuScene");
+    }
+}
