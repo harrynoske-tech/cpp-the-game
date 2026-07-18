a7fd66a7b12f80b535e61bdcfdc1decc97656ae9
harrynoske-tech <harry.noske@gmail.com>
2026-07-18 16:57:35 +1000

    Update GameScene.js

--- scenes/GameScene.js
+++ scenes/GameScene.js
@@ -1,17 +1,23 @@
 class GameScene extends Phaser.Scene {
+
     constructor() {
         super("GameScene");
     }
 
     create() {
-        const { width, height } = this.scale;
 
-        this.cameras.main.setBackgroundColor("#2b2b2b");
+        this.cameras.main.setBackgroundColor(0x202020);
 
-        this.add.text(width / 2, height / 2, "GAME SCENE", {
-            fontFamily: "Arial",
-            fontSize: "42px",
-            color: "#ffffff"
-        }).setOrigin(0.5);
+        const width = this.scale.width;
+        const height = this.scale.height;
+
+        this.add.circle(
+            width / 2,
+            height / 2,
+            18,
+            0x2ecc71
+        );
+
     }
+
 }
