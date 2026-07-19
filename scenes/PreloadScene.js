859ae391156563281faede5838633a245952899a
harrynoske-tech <harry.noske@gmail.com>
2026-07-19 18:27:08 +1000

    Update PreloadScene.js

--- scenes/PreloadScene.js
+++ scenes/PreloadScene.js
@@ -11,6 +11,26 @@ class PreloadScene extends Phaser.Scene {
             "assets/backgrounds/hotel-arrival.png"
         );
 
+        this.load.image(
+            "bodyguard-front",
+            "assets/sprites/player/bodyguard_front.png"
+        );
+
+        this.load.image(
+            "bodyguard-back",
+            "assets/sprites/player/bodyguard_back.png"
+        );
+
+        this.load.image(
+            "bodyguard-left",
+            "assets/sprites/player/bodyguard_left.png"
+        );
+
+        this.load.image(
+            "bodyguard-right",
+            "assets/sprites/player/bodyguard_right.png"
+        );
+
     }
 
     create() {
